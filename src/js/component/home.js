6bc368fc4a354b6da61d015b86e61e15bcfa28b0
Karla9191 <marietha2014@gmail.com>
2021-03-13 01:39:43 +0000

    final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,41 +5,39 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	let [option1, setOption1] = useState("Apagado");
+	let [option2, setOption2] = useState("Apagado");
+	let [option3, setOption3] = useState("Apagado");
 
-    let [option1, setOption1] = useState ("Apagado");
-    let [option2, setOption2] = useState ("Apagado");
-    let [option3, setOption3] = useState ("Apagado");
+	function optionA() {
+		if (option1 == "Apagado") {
+			setOption1("Encendido");
+			setOption2("Apagado");
+			setOption3("Apagado");
+		}
+	}
 
-    function optionA(){
-        if (option1 == "Apagado"){
-            setOption1("Encendido");
-            setOption2("Apagado");
-            setOption3("Apagado");
-        }
-    }
+	function optionB() {
+		if (option2 == "Apagado") {
+			setOption1("Apagado");
+			setOption2("Encendido");
+			setOption3("Pagado");
+		}
+	}
 
-    function optionB(){
-        if (option2 == "Apagado"){
-            setOption1("Apagado");
-            setOption2("Encendido");
-            setOption3("Pagado");
-        }
-    }
-
-    function optionC(){
-        if (option2 == "Apagado"){
-            setOption1("Apagado");
-            setOption2("Apagado");
-            setOption3("Encendido");
-        }
-    }
-
+	function optionC() {
+		if (option3 == "Apagado") {
+			setOption1("Apagado");
+			setOption2("Apagado");
+			setOption3("Encendido");
+		}
+	}
 
 	return (
-        <div className="text-center mt-5">
-            <button onClick={option1}>{option1}</button>
-            <button onClick={option2}>{option2}</button>
-            <button onClick={option3}>{option3}</button>
+		<div className="button text-center mt-5">
+			<button className="btn-1 btn-sm" onClick={optionA}></button>
+			<button className="btn-2 btn-sm" onClick={optionB}></button>
+			<button className="btn-3 btn-sm" onClick={optionC}></button>
 		</div>
 	);
 }
