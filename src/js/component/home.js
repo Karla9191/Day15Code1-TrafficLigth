dcfb65828debac9ef00a81bfc0810cd22b095ce9
Karla9191 <marietha2014@gmail.com>
2021-03-13 00:22:34 +0000

    cambios

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -13,13 +13,33 @@ export function Home() {
     function optionA(){
         if (option1 == "Apagado"){
             setOption1("Encendido");
+            setOption2("Apagado");
+            setOption3("Apagado");
         }
     }
-	return (
-		
-        
 
+    function optionB(){
+        if (option2 == "Apagado"){
+            setOption1("Apagado");
+            setOption2("Encendido");
+            setOption3("Pagado");
+        }
+    }
+
+    function optionC(){
+        if (option2 == "Apagado"){
+            setOption1("Apagado");
+            setOption2("Apagado");
+            setOption3("Encendido");
+        }
+    }
+
+
+	return (
         <div className="text-center mt-5">
+            <button onClick={option1}>{option1}</button>
+            <button onClick={option2}>{option2}</button>
+            <button onClick={option3}>{option3}</button>
 		</div>
 	);
 }
