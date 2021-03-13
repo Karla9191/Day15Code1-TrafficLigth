1debf48de477de53d6e429af5446c271ba3bff36
Karla9191 <marietha2014@gmail.com>
2021-03-13 00:19:48 +0000

    cambios

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+
+    let [option1, setOption1] = useState ("Apagado");
+    let [option2, setOption2] = useState ("Apagado");
+    let [option3, setOption3] = useState ("Apagado");
+
+    function optionA(){
+        if (option1 == "Apagado"){
+            setOption1("Encendido");
+        }
+    }
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+        
+
+        <div className="text-center mt-5">
 		</div>
 	);
 }
