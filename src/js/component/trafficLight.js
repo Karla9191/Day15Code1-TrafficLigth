8b2127a74d6235cd42860723eb16711f72fc9111
Karla9191 <marietha2014@gmail.com>
2021-03-15 00:06:33 +0000

    cambios

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 //create your first component
 export function TrafficLight() {
 	const [count, setCount] = useState(0);
+
 	let [option1, setOption1] = useState("Apagado");
 	let [option2, setOption2] = useState("Apagado");
 	let [option3, setOption3] = useState("Apagado");
@@ -34,9 +35,9 @@ export function TrafficLight() {
 	return (
 		<div className="button text-center mt-5">
 			<div className="btnAll">
-				<button className="btn-1 btn-sm" onClick={optionA}></button>
-				<button className="btn-2 btn-sm" onClick={optionB}></button>
-				<button className="btn-3 btn-sm" onClick={optionC}></button>
+				<button className="btn-1 btn-sm" onClick={}></button>
+				<button className="btn-2 btn-sm" onClick={}></button>
+				<button className="btn-3 btn-sm" onClick={}></button>
 			</div>
 			<p>You clicked {count} times</p>
 			<button onClick={() => setCount(count + 1)}>Click me</button>
