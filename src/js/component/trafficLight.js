59da9aedd1d76f72e1e4b6fe38e9005e7a921bb4
Karla9191 <marietha2014@gmail.com>
2021-04-02 00:51:51 +0000

    cambios

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -4,47 +4,44 @@ document.title = "Traffic Light";
 
 //create your first component
 export function TrafficLight() {
-	let [color1, setColor1] = useState(false);
-	let [color2, setColor2] = useState(false);
-	let [color3, setColor3] = useState(false);
+	let [colorRed, setColorRed] = useState("red");
+	let [colorYellow, setColorYellow] = useState("yellow");
+	let [colorGreen, setColorGreen] = useState("green");
 
-	function change1() {
-		if (color1 == false) {
-			setColor1(true);
-			setColor2(false);
-			setColor3(false);
+	function changeRed() {
+		if (colorRed == false) {
+			setColorRed(true);
+			setColorYellow(false);
+			setColorGreen(true);
 		} else {
-			setColor1(false);
+			setColorRed(false);
 		}
 	}
-	function change2() {
-		if (color2 == false) {
-			setColor1(false);
-			setColor2(true);
-			setColor3(false);
+
+	function changeYellow() {
+		if (colorYellow == false) {
+			setColorRed(false);
+			setColorYellow(true);
+			setColorGreen(false);
 		} else {
-			setColor2(false);
+			setColorYellow(false);
 		}
 	}
-	function change3() {
-		if (color3 == false) {
-			setColor1(false);
-			setColor2(false);
-			setColor3(true);
+	function changeGreen() {
+		if (colorGreen == false) {
+			setColorRed(false);
+			setColorYellow(false);
+			setColorGreen(true);
 		} else {
-			setColor3(false);
+			setColorGreen(false);
 		}
 	}
 
 	return (
 		<div className="container">
-			<div className={"btn-R"} onClick={change1}></div>
-			<div className={"btn-Y"} onClick={change3}></div>
-			<div className={"btn-G"} onClick={change3}></div>
+			<div className={"red btnRYG"} onClick={changeRed}></div>
+			<div className={"yellow btnRYG "} onClick={changeYellow}></div>
+			<div className={"green btnRYG "} onClick={changeGreen}></div>
 		</div>
 	);
-	/*
-    {color1 ? <ShowColor /> : null}
-	function ShowColor() {}
-    */
 }
