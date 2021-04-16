0d8139457fd4fa16430fd8517b39a680dd4bffb8
Karla9191 <marietha2014@gmail.com>
2021-04-16 17:21:02 +0000

    ñldskñlflk

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,47 +1,4 @@
-import React from "react";
-
-export default class TrafficLight extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			clickedLight: null
-		};
-	}
-
-	render() {
-		let redNewClass = "";
-		if (this.state.clickedLight == "red") redNewClass = "selected";
-		let yellowNewClass = "";
-		if (this.state.clickedLight == "yellow") yellowNewClass = "selected";
-		let greenNewClass = "";
-		if (this.state.clickedLight == "green") greenNewClass = "selected";
-
-		return (
-			<div>
-				<div id="trafficTop"></div>
-				<div id="contenedor">
-					<div
-						className={"red light " + redNewClass}
-						onClick={() =>
-							this.setState({ clickedLight: "red" })
-						}></div>
-					<div
-						className={"yellow light " + yellowNewClass}
-						onClick={() =>
-							this.setState({ clickedLight: "yellow" })
-						}></div>
-					<div
-						className={"green light " + greenNewClass}
-						onClick={() =>
-							this.setState({ clickedLight: "green" })
-						}></div>
-				</div>
-			</div>
-		);
-	}
-}
-
-/*
+import React, { useState } from "react";
 //create your first component
 export function TrafficLight() {
 	let [colorRed, setColorRed] = useState(false);
@@ -85,4 +42,3 @@ export function TrafficLight() {
 		</div>
 	);
 }
-*/
